ab583f940b277c30e89634e0b5c3d31f223f7633
WildWolf <magnus1997.mho@gmail.com>
2022-03-21 17:54:21 +0100

    clean up main code

---
+++ BSNO-DiscordBot/Events.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using NLog;
+
+namespace BSNO
+{
+    public static class Events
+    {
+        public static Task ClientOnGuildScheduledEvent(SocketGuildEvent arg)
+        {
+            return Task.CompletedTask;
+        }
+
+        public static Task ClientOnLog(LogMessage arg)
+        {
+            var level = arg.Severity switch
+            {
+                LogSeverity.Debug => LogLevel.Debug,
+                LogSeverity.Critical => LogLevel.Fatal,
+                LogSeverity.Error => LogLevel.Error,
+                LogSeverity.Warning => LogLevel.Warn,
+                LogSeverity.Info => LogLevel.Info,
+                LogSeverity.Verbose => LogLevel.Trace,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+            Program.Instance.Logger.Log(level, arg.ToString(null, true, false));
+            return Task.CompletedTask;
+        }
+
+        public static async Task ClientOnReady()
+        {
+            var list = (await Program.Instance.InteractionService.AddModulesAsync(Assembly.GetExecutingAssembly(), Program.Instance.Provider)).ToArray();
+            foreach (var guild in Program.Instance.Client.Guilds)
+            {
+                await Program.Instance.InteractionService.AddModulesToGuildAsync(guild, true, list);
+            }
+        }
+
+        public static Task ClientOnInviteCreated(SocketInvite arg)
+        {
+            return Task.CompletedTask;
+        }
+
+        public static Task ClientOnPresenceUpdated(SocketUser arg1, SocketPresence arg2, SocketPresence arg3)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
--- BSNO-DiscordBot/Program.cs
+++ BSNO-DiscordBot/Program.cs
@@ -11,17 +11,23 @@ namespace BSNO
 {
     public class Program
     {
-        public static Logger Logger = null!;
-        public static DiscordSocketClient Client = null!;
-        public static IServiceProvider Provider = null!;
-        private InteractionService _interactionService = null!;
+        public static Program Instance = null!;
+        public readonly Logger Logger;
+        public readonly DiscordSocketClient Client = new (new DiscordSocketConfig
+        {
+            GatewayIntents = GatewayIntents.All,
+            AlwaysDownloadUsers = true
+        });
+        public readonly IServiceProvider Provider;
+        public readonly InteractionService InteractionService;
 
         public Program()
         {
+            Instance = this;
             DotEnv.Load();
-            Provider = new ServiceCollection()
-                .AddLogging()
-                .BuildServiceProvider();
+            Logger = LogManager.GetCurrentClassLogger();
+            Provider = Configure(new ServiceCollection());
+            InteractionService = new InteractionService(Client);
         }
 
         public static void Main(string[] args)
@@ -29,25 +35,28 @@ namespace BSNO
             new Program().MainAsync().GetAwaiter().GetResult();
         }
 
+        public IServiceProvider Configure(ServiceCollection collection)
+        {
+            collection.AddLogging();
+
+            return collection.BuildServiceProvider();
+        }
+
         private async Task MainAsync()
         {
-            Logger = LogManager.GetCurrentClassLogger();
+            #region Discord Events assign
+            Client.Log += Events.ClientOnLog;
+            Client.SlashCommandExecuted += async command => 
+                await InteractionService.ExecuteCommandAsync(new SocketInteractionContext<SocketSlashCommand>(Client, command), Provider);
+            Client.Ready += Events.ClientOnReady;
+            Client.PresenceUpdated += Events.ClientOnPresenceUpdated;
+            Client.InviteCreated += Events.ClientOnInviteCreated;
+            Client.GuildScheduledEventStarted += Events.ClientOnGuildScheduledEvent;
+            Client.GuildScheduledEventCreated += Events.ClientOnGuildScheduledEvent;
+            Client.GuildScheduledEventCompleted += Events.ClientOnGuildScheduledEvent;
+            Client.GuildScheduledEventCancelled += Events.ClientOnGuildScheduledEvent;
+            #endregion
 
-            Client = new DiscordSocketClient(new DiscordSocketConfig
-            {
-                GatewayIntents = GatewayIntents.All,
-                AlwaysDownloadUsers = true
-            });
-            _interactionService = new InteractionService(Client);
-            Client.Log += ClientOnLog;
-            Client.SlashCommandExecuted += ClientOnSlashCommandExecuted;
-            Client.Ready += ClientOnReady;
-            Client.PresenceUpdated += ClientOnPresenceUpdated;
-            Client.InviteCreated += ClientOnInviteCreated;
-            Client.GuildScheduledEventStarted += ClientOnGuildScheduledEvent;
-            Client.GuildScheduledEventCreated += ClientOnGuildScheduledEvent;
-            Client.GuildScheduledEventCompleted += ClientOnGuildScheduledEvent;
-            Client.GuildScheduledEventCancelled += ClientOnGuildScheduledEvent;
             await Client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("BOT_KEY"));
             await Client.StartAsync();
             while (Console.ReadKey().Key != ConsoleKey.Q)
@@ -59,52 +68,6 @@ namespace BSNO
             {
                 Task.Delay(10).GetAwaiter().GetResult();
             }
-        }
-
-        private Task ClientOnGuildScheduledEvent(SocketGuildEvent arg)
-        {
-            return Task.CompletedTask;
-        }
-
-        private async Task ClientOnSlashCommandExecuted(SocketSlashCommand arg)
-        {
-            var ctx = new SocketInteractionContext<SocketSlashCommand>(Client, arg);
-            await _interactionService.ExecuteCommandAsync(ctx, Provider);
-        }
-
-        private Task ClientOnLog(LogMessage arg)
-        {
-            var level = arg.Severity switch
-            {
-                LogSeverity.Debug => LogLevel.Debug,
-                LogSeverity.Critical => LogLevel.Fatal,
-                LogSeverity.Error => LogLevel.Error,
-                LogSeverity.Warning => LogLevel.Warn,
-                LogSeverity.Info => LogLevel.Info,
-                LogSeverity.Verbose => LogLevel.Trace,
-                _ => throw new ArgumentOutOfRangeException()
-            };
-            Logger.Log(level, arg.ToString(null, true, false));
-            return Task.CompletedTask;
-        }
-
-        private async Task ClientOnReady()
-        {
-            var list = (await _interactionService.AddModulesAsync(Assembly.GetExecutingAssembly(), Provider)).ToArray();
-            foreach (var guild in Client.Guilds)
-            {
-                await _interactionService.AddModulesToGuildAsync(guild, true, list);
-            }
-        }
-
-        private Task ClientOnInviteCreated(SocketInvite arg)
-        {
-            return Task.CompletedTask;
-        }
-
-        private Task ClientOnPresenceUpdated(SocketUser arg1, SocketPresence arg2, SocketPresence arg3)
-        {
-            return Task.CompletedTask;
         }
     }
 }
